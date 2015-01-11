3c23601ccd874dcd1aae53fcb722656d775c1d1a
Robert A. Schreiber <mail@robert-schreiber.de>
2015-01-11 18:13:50 +0100

    added local

--- GitExperimentProject/src/com/rsr/exp/git/GitExperimentProjectClass2.java
+++ GitExperimentProject/src/com/rsr/exp/git/GitExperimentProjectClass2.java
@@ -1,5 +1,12 @@
 package com.rsr.exp.git;
 
 public class GitExperimentProjectClass2 {
-
+	
+	  public void addedNewFeatureLocal() {
+		    
+	  }
+	  
+	  public void addedNewFeatureLocal2() {
+		    
+	  }
 }
